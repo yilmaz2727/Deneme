81f47bd1a12907194217f905377a7ead33516876
yilmaz2727 <hy97556@gmail.com>
2024-11-04 23:18:26 +0300

    Update hello.cpp
     my_grade_calculator.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,22 +1,21 @@
-
-
 #include <iostream>
 using namespace std;
-int  main()
+
+int main()
 {
-
-
-int number1 ;
-number1 = 5 ;
-
-
- cout << "hello wod" << number1 << endl ;
-return 0; 
-
-
-
-
-
-
-
+	float midterm_grade, final_grade, your_avarege;
+	cout << "Input your midterm grade:\n ";
+	cin >> midterm_grade;
+	cout << " input your final grade: " << endl ;
+	cin >> final_grade;
+	your_avarege = (midterm_grade * 0.4) + (final_grade * 0.6);
+	cout << "avarage is " << your_avarege << endl;
+	if (your_avarege >= 50)
+		cout << "passed..";
+	if (40 < your_avarege || your_avarege < 50)
+		cout << "depending on others grades";
+	else
+		cout << "failed... " << endl;
+		
 }
+
